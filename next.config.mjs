b7f98ba04a971a6efb390ccbadc8489bd37fd930
gokinsonne <gok1ns0n@gmail.com>
2025-09-06 14:37:06 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,5 +10,36 @@ const nextConfig = {
     unoptimized: true,
   },
 }
+async redirects() {
+    return [
+      {
+        source: "/play",
+        destination: "https://lkvn.cc/a03dba",
+        permanent: true,
+      },
+      {
+        source: "/demo",
+        destination: "https://lkvn.cc/a03dba",
+        permanent: true,
+      },
+      {
+        source: "/bonus",
+        destination: "https://lkvn.cc/a03dba",
+        permanent: true,
+      },
+      {
+        source: "/app",
+        destination: "https://lkvn.cc/a03dba",
+        permanent: true,
+      },
+      {
+        source: "/download-vpn",
+        destination: "https://lkvn.cc/a03dba",
+        permanent: true,
+      },
+    ];
+  },
+};
+
 
 export default nextConfig
